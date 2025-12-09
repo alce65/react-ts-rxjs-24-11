60e660cce469330bfbe0cd6ced880cd1fd7b32b9
Alejandro Cerezo <alce65@hotmail.es>
2025-12-09 18:37:07 +0100

    Fix previous problems

--- projects/demo3-rx/src/components/features/components/counter/counter.tsx
+++ projects/demo3-rx/src/components/features/components/counter/counter.tsx
@@ -1,17 +1,17 @@
 import { Card } from '@components/core/card/card';
 import { useObservableV3 } from '@components/features/hooks/rx/use-observable-v3';
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { fromEvent, Observable, scan } from 'rxjs';
 
 export const Counter: React.FC = () => {
     const buttonRef = useRef<HTMLButtonElement | null>(null);
-    
-    const factory = (): Observable<number> => {
+
+    const factory = useCallback((): Observable<number> => {
         const button = buttonRef.current as HTMLButtonElement;
         return fromEvent<MouseEvent>(button, 'click').pipe(
             scan((acc) => acc + 1, 0)
         );
-    };
+    }, []);
 
     const [count] = useObservableV3<number>(factory, 0);
 
